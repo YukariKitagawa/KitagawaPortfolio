51e445fe44450056d68f865ad5c4f6b1e178adcd
YukariKitagawa <kitagawayukari0919@gmail.com>
2025-02-07 22:24:22 +0900

    ビルド用に修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/KitagawaPortfolio" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/KitagawaPortfolio" : "",
+}
 
-export default nextConfig;
+export default nextConfig
